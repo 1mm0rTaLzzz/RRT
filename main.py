f32986ee32a0c3a1b740f2a2cbdc8e8424f5701e
1mm0rTaLzzz <88653641+1mm0rTaLzzz@users.noreply.github.com>
2023-03-22 23:28:06 +0300

    Add files via upload
    changed the drawing type from building a rectangle to a line and reworked the collision

--- main.py
+++ main.py
@@ -2,14 +2,12 @@ import pygame
 import random
 import math
 
-
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 YELLOW = (255, 255, 0)
-
 
 WIDTH = 800
 HEIGHT = 600
@@ -31,6 +29,7 @@ class RRT:
         self.obstacles = obstacles
         self.nodes = [start]
         self.step_size = 20
+        self.final_step = 50
 
     def find_path(self):
         for i in range(10000):
@@ -44,14 +43,14 @@ class RRT:
                 (rand_node.x - nearest_node.x) ** 2 + (rand_node.y - nearest_node.y) ** 2),
                             nearest_node.y + self.step_size * (rand_node.y - nearest_node.y) / math.sqrt(
                                 (rand_node.x - nearest_node.x) ** 2 + (rand_node.y - nearest_node.y) ** 2))
-            if self.check_collision(nearest_node, new_node):
+            if not self.collision(nearest_node, new_node, self.obstacles):
                 new_node.parent = nearest_node
                 self.nodes.append(new_node)
                 print(f"Random node position: ({rand_node.x}, {rand_node.y})")
                 print(f"Nearest node position: ({nearest_node.x}, {nearest_node.y})")
                 print(f"New node position: ({new_node.x}, {new_node.y})")
 
-            if math.sqrt((new_node.x - self.goal.x) ** 2 + (new_node.y - self.goal.y) ** 2) < self.step_size:
+            if math.sqrt((new_node.x - self.goal.x) ** 2 + (new_node.y - self.goal.y) ** 2) < self.final_step:
                 final_node = Node(self.goal.x, self.goal.y)
                 final_node.parent = new_node
                 self.nodes.append(final_node)
@@ -61,55 +60,58 @@ class RRT:
                 return [(node.x, node.y) for node in path[::-1]]
         return None
 
-    def check_collision(self, node1, node2):
-        for obstacle in self.obstacles:
-            if obstacle.collidepoint(node1.x, node1.y) or obstacle.collidepoint(node2.x, node2.y):
-                return False
-        return True
-
     def draw(self, screen):
         for node in self.nodes:
             pygame.draw.circle(screen, YELLOW, (node.x, node.y), 3)
             if node.parent is not None:
                 pygame.draw.line(screen, WHITE, (node.x, node.y), (node.parent.x, node.parent.y), 2)
-        for obstacle in self.obstacles:
-            pygame.draw.rect(screen, BLUE, obstacle)
         pygame.draw.circle(screen, RED, (self.start.x, self.start.y), 10)
         pygame.draw.circle(screen, GREEN, (self.goal.x, self.goal.y), 10)
+
+    def collision(self, src, dst, obstacles):
+        vx, vy = rrt.normalize(dst.x - src.x, dst.y - src.y)
+        curr = [src.x, src.y]
+        while rrt.dist(curr, dst) > 1:
+            intCurr = int(curr[0]), int(curr[1])
+            try:
+                if obstacles.get_at(intCurr) == BLUE:
+                    return True
+            except Exception:
+                pass
+            curr[0] += vx
+            curr[1] += vy
+        return False
+
+    def dist(self, p1, p2):
+        return math.hypot(p2.x - p1[0], p2.y - p1[1])
+
+    def normalize(self, vx, vy):
+        norm = math.sqrt(vx * vx + vy * vy)
+        if norm > 1e-6:
+            vx /= norm
+            vy /= norm
+        return vx, vy
 
 
 # Define function to create obstacles
 def create_obstacles():
-    obstacles = []
-    drawing = False
-    start_pos = None
     done = False
     while not done:
         for event in pygame.event.get():
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE:
-                    done = True
-            elif event.type == pygame.MOUSEBUTTONDOWN:
-                pos = pygame.mouse.get_pos()
-                if not drawing:
-                    start_pos = pos
-                    drawing = True
-                else:
-                    x, y = min(start_pos[0], pos[0]), min(start_pos[1], pos[1])
-                    w, h = abs(start_pos[0] - pos[0]), abs(start_pos[1] - pos[1])
-                    obstacles.append(pygame.Rect(x, y, w, h))
-                    drawing = False
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                done = True
+            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                flStartDraw = True
             elif event.type == pygame.MOUSEMOTION:
-                if drawing:
-                    for obstacle in obstacles:
-                        pygame.draw.rect(screen, BLUE, obstacle)
-                    # Draw new rectangle
-                    pos = pygame.mouse.get_pos()
-                    x, y = min(start_pos[0], pos[0]), min(start_pos[1], pos[1])
-                    w, h = abs(start_pos[0] - pos[0]), abs(start_pos[1] - pos[1])
-                    pygame.draw.rect(screen, BLUE, (x, y, w, h))
-        pygame.display.update()
-    return obstacles
+                if flStartDraw:
+                    pos = event.pos
+                    pygame.draw.circle(screen, BLUE, pos, 10)
+                    pygame.display.update()
+            elif event.type == pygame.MOUSEBUTTONUP and event.button == 1:
+                flStartDraw = False
+    pygame.image.save(screen, 'map.png')
+    obstaclesSurface = pygame.image.load('map.png')
+    return obstaclesSurface
 
 
 # Define function to get the start and end points from user
@@ -159,14 +161,11 @@ while running:
 
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_TAB:
-                screen.fill(BLACK)
-
                 path = rrt.find_path()
-
                 rrt.draw(screen)
 
                 if path is not None:
-                    pygame.draw.aalines(screen, BLUE, False, path, 3)
+                    pygame.draw.lines(screen, BLUE, False, path, 3)
 
                 pygame.display.update()
 
